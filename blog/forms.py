9ecc85c3d019c8fee8b4556ff26594c7d62e0565
zgna <zgna2015@gmail.com>
2016-05-29 20:54:54 +0300

    Changed blog/forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-    from .models import Post
+from .models import Post
+    
+class PostForm(forms.ModelForm):
 
-    class PostForm(forms.ModelForm):
-
-        class Meta:
-            model = Post
-            fields = ('title', 'text',)
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
